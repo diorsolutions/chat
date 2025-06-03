af8b13fbfc6af0cd20c54bf79819de7f73ee0a2a
diorsolutions <diorliders@gmail.com>
2025-06-03 23:40:17 +0500

    finished

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { io } from "socket.io-client";
+import { io } from "socket.io-client"; // ✅ faqat shu kerak
 import styles from "./ChatPage.module.css";
-import ChatUsersInfo from "./ChatUsersInfo"; // ✅ yangi komponentni import qilamiz
-import io from "socket.io-client";
+import ChatUsersInfo from "./ChatUsersInfo";
 
 
 // const socket = io(process.env.REACT_APP_SOCKET_SERVER);
